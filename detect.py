89c7859b7325ae1c1e07846483fcbc0d2cc9a35c
neuralassembly <neuralassembly@gmail.com>
2021-04-18 18:28:27 +0900

    Add support for web camera

--- detect.py
+++ detect.py
@@ -17,7 +17,7 @@ flags.DEFINE_string('weights', './checkpoints/yolov3.tf',
 flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
 flags.DEFINE_string('image', './data/girl.png', 'path to input image')
 flags.DEFINE_string('output', './output.jpg', 'path to output image')
-
+flags.DEFINE_boolean('webcam', False, 'image or webcam')
 
 def main(_argv):
     if FLAGS.tiny:
@@ -31,25 +31,46 @@ def main(_argv):
     class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
     logging.info('classes loaded')
 
-    img = tf.image.decode_image(open(FLAGS.image, 'rb').read(), channels=3)
-    img = tf.expand_dims(img, 0)
-    img = transform_images(img, 416)
+    if not FLAGS.webcam:
 
-    t1 = time.time()
-    boxes, scores, classes, nums = yolo(img)
-    t2 = time.time()
-    logging.info('time: {}'.format(t2 - t1))
+        img = tf.image.decode_image(open(FLAGS.image, 'rb').read(), channels=3)
+        img = tf.expand_dims(img, 0)
+        img = transform_images(img, 416)
 
-    logging.info('detections:')
-    for i in range(nums[0]):
-        logging.info('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-                                           scores[0][i].numpy(),
-                                           boxes[0][i].numpy()))
+        t1 = time.time()
+        boxes, scores, classes, nums = yolo(img)
+        t2 = time.time()
+        logging.info('time: {}'.format(t2 - t1))
+    
+        logging.info('detections:')
+        for i in range(nums[0]):
+            logging.info('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                               scores[0][i].numpy(),
+                                               boxes[0][i].numpy()))
 
-    img = cv2.imread(FLAGS.image)
-    img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-    cv2.imwrite(FLAGS.output, img)
-    logging.info('output saved to: {}'.format(FLAGS.output))
+        img = cv2.imread(FLAGS.image)
+        img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
+        cv2.imwrite(FLAGS.output, img)
+        logging.info('output saved to: {}'.format(FLAGS.output))
+
+    else:
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = tf.expand_dims(img, 0)
+            img = transform_images(img, 416)
+            boxes, scores, classes, nums = yolo(img)
+
+            frame = draw_outputs(frame, (boxes, scores, classes, nums), class_names)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+
 
 if __name__ == '__main__':
     try:
